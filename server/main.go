beeb08df54a7f79b5f64fed430fac52e6348c3ce
MCarbono <carbono.marcelo@gmail.com>
2022-11-28 19:18:15 -0300

    refact: seed db table, init func

--- server/main.go
+++ server/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("sqlite3", "./dollar_cotation.db")
+	db, err := initDB()
 	if err != nil {
 		panic(err)
 	}
@@ -21,4 +21,29 @@ func main() {
 	handler := handler.DollarCotationHandler{DollarCotationUseCase: usecase.DollarCotationUseCase{DollarCotationRepository: *repository}}
 	http.HandleFunc("/cotacao", handler.Get)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func initDB() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", "dollar_cotations.db")
+	if err != nil {
+		return nil, err
+	}
+	_, err = db.Exec(`CREATE TABLE cotations (
+		id varchar(255) NOT NULL,
+		code varchar(255),
+		codein varchar(255),
+		name varchar(255),
+		high varchar(255),
+		low varchar(255),
+		varBid varchar(255),
+		pctChange varchar(255),
+		bid varchar(255),
+		ask varchar(255),
+		timestamp TIMESTAMP,
+		create_date DATE(255),
+		PRIMARY KEY (id));`)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
